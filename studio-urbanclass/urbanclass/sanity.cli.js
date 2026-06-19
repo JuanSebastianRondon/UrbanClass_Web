358a69754591adc4f1d3058c02875ad28e296700
JuanSebastianRondon <megapikachu03@gmail.com>
2026-06-18 20:08:08 -0500

    agregado appId al cli config

--- studio-urbanclass/urbanclass/sanity.cli.js
+++ studio-urbanclass/urbanclass/sanity.cli.js
@@ -5,11 +5,12 @@ export default defineCliConfig({
     projectId: 'y67i0hab',
     dataset: 'production'
   },
-  deployment: {
+ deployment: {
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'qia0ot9qo2caql2b8vjrhlqx',
   }
 })
